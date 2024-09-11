dd1b73645ab8c741517bda8dade016ff61ef1286
SiD0M <simon1.wasner@st.oth-regensburg.de>
2024-09-11 13:02:33 +0200

    first draft with right story

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 import csv
 
-file_path = "static/csv/futureskills.csv"
+#file_path = "static/csv/futureskills_ks.csv"
+file_path = "static/csv/futureskills_bs.csv"
 
 def read_csv():
     data = []
@@ -8,9 +9,11 @@ def read_csv():
         reader = csv.DictReader(file)
         for row in reader:
             data.append({
+                'id': row['id'],
                 'title': row['skill'],
                 'definition': row['definition'],
-                'story': row['story']
+                'small_story': row['small_story'],
+                'big_story': row['big_story']
                 })
     return data
 
